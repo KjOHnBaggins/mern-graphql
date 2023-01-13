2475ebcabda5cf24c19fb4e77f7e2683ae642b45
KjOHnBaggins <okenshieldak47@gmail.com>
2023-01-13 12:02:06 +0630

    schema, express

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "Super Action Movie",
+    description:
+      "The greatest super star Johny Cage embarks on an adventure with a strange thunder flashing man and a brave combatant who would later become the love of his life",
+    status: "He's directing it",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Extract Mission",
+    description:
+      "Special Forces Commander Cage plans to rescue her husband Johny Cage from the rogue elder god Shinnok",
+    status: "Classified",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Training for Mortal Kombat",
+    description:
+      "The future fire god Liu Kang trains for the great tournament in order to save Earthrealm",
+    status: "Training in Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Accompany Liu Kang",
+    description:
+      "Kung Lao trains with Liu Kang to participate in the Mortal Kombat tournament",
+    status: "Training in Progress",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Stop the Netherealm invasion",
+    description:
+      "Raiden mentors both pupils to participate in the Mortal Kombat tournament",
+    status: "Training in Progress",
+  },
+];
+
+// Clients
+const clients = [
+  {
+    id: "1",
+    name: "Johny Cage",
+    email: "cagesuperstar@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Cassandra Cage",
+    email: "cagespecialforces@gmail.com",
+    phone: "223-567-3322",
+  },
+  {
+    id: "3",
+    name: "Liu Kang",
+    email: "liukangthechampion@gmail.com",
+    phone: "324-331-4333",
+  },
+  {
+    id: "4",
+    name: "Kung Lao",
+    email: "kunglaotherazorrimmed@gmail.com",
+    phone: "344-562-6787",
+  },
+  {
+    id: "5",
+    name: "Raiden",
+    email: "thethundergod@gmail.com",
+    phone: "321-468-8887",
+  },
+];
+
+module.exports = { projects, clients };
